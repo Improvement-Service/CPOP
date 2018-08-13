b5a0a68a5b1844dd26b1f68a4b02e41422b4da62
Nick Cassidy <nicholas.cassidy@improvementservice.org.uk>
2018-08-13 10:28:18 +0100

    Fix names in Shapefiles so Edinburgh and W.Isles can be selected

--- Other Code/LA Shapefiles.R
+++ Other Code/LA Shapefiles.R
@@ -6,5 +6,8 @@ shps <- readRDS("C:/Users/cassidy.nicholas/OneDrive - IS/WRDashboard/LAPolys.rds
 
 shps2 <- gSimplify(shps, 0.0005,TRUE)
 shps <- SpatialPolygonsDataFrame(shps2, shps@data)
+shps@data[shps@data$NAME== "City of Edinburgh", "NAME"] <- "Edinburgh, City of"
+shps@data[shps@data$NAME== "Na h-Eileanan an Iar", "NAME"] <- "Eilean Siar"
+
 
 saveRDS(shps, "C:/Users/cassidy.nicholas/OneDrive - IS/CPOP/data/LAShps.rds")
