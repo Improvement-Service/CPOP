0175139d6534d0d09ba64fda29b926154c1e21df
Nick Cassidy <nicholas.cassidy@improvementservice.org.uk>
2019-02-06 14:38:11 +0000

    Create clean data

--- Other Code/Clean data.R
+++ Other Code/Clean data.R
@@ -5,18 +5,18 @@ library(readxl)
 #use for loop to loop through and read every worksheet
 ##CPP data
 
-CPPdta <- read_excel("data/CPP Data Sept 18.xlsx",
+CPPdta <- read_excel("data/Final CPP Data - Feb 19.xlsx",
                      sheet = 2)
 
 for (i in 3:20) {
-  CPPdta2 <- read_excel("data/CPP Data Sept 18.xlsx",
+  CPPdta2 <- read_excel("data/Final CPP Data - Feb 19.xlsx",
                         sheet = i)
   CPPdta <- merge(CPPdta, CPPdta2)
 }
 
 #combine columns into 1 long variable
 CPPdta <- gather(CPPdta, Indicator_Type_Year, value, -1)
-#seperate 1 column into 3
+#separate 1 column into 3
 CPPdta <- separate(CPPdta, Indicator_Type_Year, c("Indicator", "Type", "Year"), sep = "_")
 #rename 1st variable
 colnames(CPPdta)[1] <- "CPP"
@@ -30,21 +30,22 @@ CPPdta[CPPdta$Indicator =="Emergency Admissions","IndicatorFullName"] <- "Emerge
 CPPdta[CPPdta$Indicator =="Out of Work Benefits","IndicatorFullName"] <- "Out of Work Benefits (%)"
 CPPdta[CPPdta$Indicator =="Positive Destinations","IndicatorFullName"] <- "Positive Destinations (%)"     
 CPPdta[CPPdta$Indicator =="Depopulation","IndicatorFullName"] <- "Depopulation Index"
+CPPdta[CPPdta$Indicator == "Attainment", "IndicatorFullName"] <- "Average Highest Attainment"
 #save CSV data file
 write_excel_csv(CPPdta ,path = "data/CPPcleandata.csv")
 
 ##IGZ data
-IGZdta <- read_excel("data/IGZ Data Sept 18.xlsx",
+IGZdta <- read_excel("data/Final IGZ Data - Feb 19.xlsx",
                      sheet = 2)
 for (i in 3:10) {
-  IGZdta2 <- read_excel("data/IGZ Data Sept 18.xlsx",
+  IGZdta2 <- read_excel("data/Final IGZ Data - Feb 19.xlsx",
                         sheet = i)
   IGZdta <- merge(IGZdta, IGZdta2)
 }
 
 #combine columns into 1 long variable
 IGZdta <- gather(IGZdta, Indicator_Type_Year, value, -1,-2,-3,-4,-5)
-#seperate 1 column into 3
+#separate 1 column into 3
 IGZdta <- separate(IGZdta, Indicator_Type_Year, c("Indicator", "Type", "Year"), sep = "_")
 ##rename indicators- in new column
 IGZdta$IndicatorFullName <- IGZdta$Indicator
@@ -55,23 +56,23 @@ IGZdta[IGZdta$Indicator =="Emergency Admissions","IndicatorFullName"] <- "Emerge
 IGZdta[IGZdta$Indicator =="Out of Work Benefits","IndicatorFullName"] <- "Out of Work Benefits (%)"
 IGZdta[IGZdta$Indicator =="Positive Destinations","IndicatorFullName"] <- "Positive Destinations (%)"     
 IGZdta[IGZdta$Indicator =="Depopulation","IndicatorFullName"] <- "Depopulation Index"
-
+IGZdta[IGZdta$Indicator == "Attainment", "IndicatorFullName"] <- "Average Highest Attainment"
 #save CSV data file
 write_excel_csv(IGZdta ,path = "data/IGZcleandata.csv")
 
 
 ##DZ data
-DZdta <- read_excel("Final DZ Data.xlsx",
+DZdta <- read_excel("data/Final DZ Data - Feb 19.xlsx",
                      sheet = 2)
 for (i in 3:7) {
-  DZdta2 <- read_excel("Final DZ Data.xlsx",
+  DZdta2 <- read_excel("data/Final DZ Data - Feb 19.xlsx",
                         sheet = i)
   DZdta <- merge(DZdta, DZdta2)
 }
 
 #combine columns into 1 long variable
 DZdta <- gather(DZdta, Indicator_Type_Year, value, -1,-2,-3,-4)
-#seperate 1 column into 3
+#separate 1 column into 3
 DZdta <- separate(DZdta, Indicator_Type_Year, c("Indicator","Type","Year"), sep = "_")
 #save CSV data file
 write_excel_csv(DZdta ,path = "DZcleandata.csv")
