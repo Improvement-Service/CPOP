0c6b66659fbc1a3dd64a12e8798491011383649b
Nick Cassidy <nicholas.cassidy@improvementservice.org.uk>
2023-09-07 16:01:25 +0100

    Prepare duncan index data

--- Duncan_Index/Final code for IGZ indicators.R
+++ Duncan_Index/Final code for IGZ indicators.R
@@ -32,9 +32,9 @@ dindex <- function(x, y, sort.var){
 }
 
 ##  Step 2) Now we need to get all the data that we need from various files
-#sco06a <- read.csv('DIData/2006 IGZ1.csv')
-#sco07a <- read.csv('DIData/2007 IGZ1.csv') remove old year before adding new ones-we only want 12 years
-#sco08a <- read.csv('DIData/2008 IGZ1.csv')
+sco06a <- read.csv('DIData/2006 IGZ1.csv')
+sco07a <- read.csv('DIData/2007 IGZ1.csv') 
+sco08a <- read.csv('DIData/2008 IGZ1.csv')
 sco09a <- read.csv('DIData/2009 IGZ1.csv')
 sco10a <- read.csv('DIData/2010 IGZ1.csv')
 sco11a <- read.csv('DIData/2011 IGZ1.csv')
@@ -50,7 +50,7 @@ sco20a <- read.csv('DIData/2020 IGZ1.csv')
 
 combined.tab <- rbind(sco09a, sco10a, sco11a, sco12a, sco13a, sco14a, sco15a, sco16a, sco17a,sco18a,sco19a, sco20a)
 
-##  Step 3) Caculate the duncan index for every combination of year and ttwa / la
+##  Step 3) Calculate the duncan index for every combination of year and ttwa / la
 
 ##  LA
 la.tab <- combined.tab %>%
