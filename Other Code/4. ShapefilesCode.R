642aacaa7dec1e3947c5a2a99214533035b4bb91
Nick Cassidy <nicholas.cassidy@improvementservice.org.uk>
2023-09-13 16:33:01 +0100

    Switch from retired packages to sf

--- Other Code/4. ShapefilesCode.R
+++ Other Code/4. ShapefilesCode.R
@@ -4,9 +4,8 @@ setwd("C:/Users/cassidy.nicholas/OneDrive - IS/CPOP")
 library(readr)
 library(readxl)
 library(dplyr)
-library(rgdal)
 library(dplyr)
-library(rgeos)
+library(sf)
 #read shapefiles
 SpPolysDF<-read_rds("C:/Users/cassidy.nicholas/OneDrive - IS/Data/Shapefiles/DZ11.rds")
 
@@ -23,7 +22,7 @@ colnames(SpPolysDF@data)[12] <- "council"
 #SpPolysDF@data[SpPolysDF@data$INTZONE_NAME %in% dups, 10] <- paste(SpPolysDF@data[SpPolysDF@data$INTZONE_NAME %in% dups, 10], abbreviate(SpPolysDF@data[SpPolysDF@data$INTZONE_NAME %in% dups, 7],6), sep = " ")
 
 #Read the data zone indicator data
-indDta <- read_excel("C:/Users/cassidy.nicholas/OneDrive - IS/CPOP/data/Final data - Aug 2022/datazone data for maps.xlsx")
+indDta <- read_excel("C:/Users/cassidy.nicholas/OneDrive - IS/CPOP/data/Final data - Sep 2023/datazone data for maps.xlsx")
 SpPolysDF@data <- left_join(SpPolysDF@data, indDta[c(1,3,4,5,6,7,8)], by = c("DataZone" = "Datazone"))
 #remove old Intzone name column
 SpPolysDF@data$IZname <- SpPolysDF@data$IGZ
@@ -45,10 +44,11 @@ dta <- left_join(dta,wDNms[c(1,3)], by = c("DataZone"))
 dta$IZname <- dta$`IZ Name`
 dta <- select(dta, -"IZ Name")
 rownames(dta) <- 0:6975
-SpPolysDF <- gSimplify(SpPolysDF,  0.00005,TRUE)
-SpPolysDF <- SpatialPolygonsDataFrame(SpPolysDF, dta)
+SpPolysDF <- st_as_sf(SpPolysDF)
+sf_use_s2(FALSE)
+SpPolysDF <- st_simplify(SpPolysDF,  dTolerance = 0.5, preserveTopology = TRUE)
 #save
-saveRDS(SpPolysDF, file = "C:/Users/cassidy.nicholas/OneDrive - IS/CPOP/data/Shapes.rds")
+saveRDS(SpPolysDF, file = "C:/Users/cassidy.nicholas/OneDrive - IS/CPOP/data/Shapes2.rds")
 
 
 ## Intermediate Geography Shapes
@@ -72,8 +72,9 @@ SpPolysIZ <- spTransform(SpPolysIZ, CRS("+proj=longlat +ellps=WGS84 +datum=WGS84
 dta <- SpPolysIZ@data
 rownames(dta) <- 0:1278
 ##simplify to make the size smaller
-SpPolysIZ <- gSimplify(SpPolysIZ, 0.00005,TRUE)
-SpPolysIZ <- SpatialPolygonsDataFrame(SpPolysIZ, dta)
+SpPolysIZ <- st_as_sf(SpPolysIZ)
+sf_use_s2(FALSE)
+SpPolysIZ <- st_simplify(SpPolysIZ,  dTolerance = 0.5, preserveTopology = TRUE)
 
 #Save 
-saveRDS(SpPolysIZ, "C:/Users/cassidy.nicholas/OneDrive - IS/CPOP/data/IZshapes.rds")
+saveRDS(SpPolysIZ, "C:/Users/cassidy.nicholas/OneDrive - IS/CPOP/data/IZshapes2.rds")
